734f36e7da61a6810c4c68c1061a8aa175fbc63f
JoaoNSE <nobreenrique@hotmail.com>
2019-03-16 10:35:59 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Registro-de-Horas-de-Trabalho
---
+++ src/main/java/br/qxd/jh/registry/RegistrosDeHorasApplication.java
@@ -0,0 +1,13 @@
+package br.qxd.jh.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrosDeHorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrosDeHorasApplication.class, args);
+	}
+
+}
